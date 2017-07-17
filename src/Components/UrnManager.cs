fe48419b10efdd1529347bed383704925f3e5a74
硫酸鶏 <root@acid-chicken.com>
2017-07-17 18:03:54 +0900

    Add UrnManager

---
+++ src/Components/UrnManager.cs
@@ -0,0 +1,32 @@
+namespace ChaosGlobal.ChaosBot.Components
+{
+    using static Program;
+
+    /// <summary>
+    /// 内部 URN を管理するクラスです。
+    /// </summary>
+
+    public static class UrnManager
+    {
+        /// <summary>
+        /// 内部 URN を 汎用 URL に変換します。
+        /// </summary>
+        /// <param name="uri">検証する URI</param>
+        /// <returns>URI が内部 URN に一致する場合は汎用 URL を返し、一致しない URN であれば null を返します。それ以外の場合、入力された文字列をそのまま返します。</returns>
+        public static string ConvertUrn(this string uri)
+        {
+            if (uri?.StartsWith("urn:") ?? false) // パラメーターが URN であるかどうかを検証します。
+            { // パラメーターが URN である場合
+                switch (uri.ToLower())
+                {
+                    default: // どの内部 URN にも一致しない場合
+                        return null; // null を返します。
+                }
+            }
+            else
+            { // パラメーターが URN でない場合
+                return uri; // パラメーターをそのまま返します。
+            }
+        }
+    }
+}
